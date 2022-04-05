9fb60155e71a57302df8f86e312b680d45b800e9
Scott <smurr4@uwo.ca>
2022-04-04 20:24:39 -0400

    Fixed Gino attack
    Allows Gino to attack enemies now

--- Assets/__Scripts/UIScript.cs
+++ Assets/__Scripts/UIScript.cs
@@ -23,10 +23,17 @@ public class UIScript : MonoBehaviour {
     // Update is called once per frame
     void Update(){
         //same code from up above that way theres no need to update it in the player class
+<<<<<<< Updated upstream
         healthText.text = "HP: " + Player.health + "/100";
         qAbilityText.text = "Q: " + Player.qAbility + "    " + Player.publicDamageCooldown + " s";
         eAbilityText.text = "E: " + Player.eAbility + "    " + Player.publicInvincibilityCooldown + " s";
         shiftText.text = "Shift: " + Player.shift + "    " + Player.publicTeleportCooldown + " s";
+=======
+        healthText.text = "HP: " + Player1.health + "/"+ Player1.maxHealth;
+        qAbilityText.text = "Q: " + Player1.qAbility + "    " + Player1.publicDamageCooldown + " s";
+        eAbilityText.text = "E: " + Player1.eAbility + "    " + Player1.publicInvincibilityCooldown + " s";
+        shiftText.text = "Shift: " + Player1.shift + "    " + Player1.publicTeleportCooldown + " s";
+>>>>>>> Stashed changes
 
     }
 }
