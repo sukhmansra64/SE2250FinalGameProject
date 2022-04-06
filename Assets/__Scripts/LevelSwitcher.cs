671cbd196c2596f26bad2ed68d5b536ea10c8cfe
kenangranulo <90563140+kenangranulo@users.noreply.github.com>
2022-04-05 21:18:31 -0400

    New comments
    Comments

--- Assets/__Scripts/LevelSwitcher.cs
+++ Assets/__Scripts/LevelSwitcher.cs
@@ -5,12 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class LevelSwitcher : MonoBehaviour {
 
+    //Set the sceneCount to 0. 
     int sceneCount = 0;
 
     // Update is called once per frame
     void Update(){
+        //If the scene count is equal to 0 and the score is equal to 200
         if (sceneCount == 0 && UIScript.score == 200) {
+            //Set the sceneCount to 1. 
             sceneCount = 1;
+            //Load the midstory scene
             SceneManager.LoadScene("_Midstory");
         }
     }
--- Assets/__Scripts/Projectile.cs
+++ Assets/__Scripts/Projectile.cs
@@ -4,22 +4,22 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    //Create an instance of Vector3. 
     public Vector3 LaunchOffset;
     
 
     private void Start()
     {
+        //If the "Ranged" is equal to "Bomb" then the following code will run. 
         if (PlayerPrefs.GetString("Ranged")=="Bomb")
         {
+            //Set the direction. 
             var direction = -transform.right + Vector3.down;
+            //Add the corresponding force. 
             GetComponent<Rigidbody2D>().AddForce(direction * 8, ForceMode2D.Impulse);
         }
-        //transform.Translate(LaunchOffset);
+        //Destory the game object. 
         Destroy(gameObject, 0.5f);
     }
 
-    public void Update()
-    {
-        
-    }
 }
