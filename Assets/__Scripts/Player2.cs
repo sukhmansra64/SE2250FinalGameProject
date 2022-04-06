ae279ec70d9de0be078ed3062aba93da732cd67c
Scott <smurr4@uwo.ca>
2022-04-05 21:19:45 -0400

    Changed Scripts
    Added comments to Player2 and Player3 script and removed the ability to die from pressing P

--- Assets/__Scripts/Player2.cs
+++ Assets/__Scripts/Player2.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Player2 : ParentPlayer{
+public class Player2 : ParentPlayer {
 
-    // Start is called before the first frame update
-    void Awake(){
+    // Awake is called when the script instance is being loaded
+    void Awake() {
 
         //initializes player health depending on the scene
         if (this.gameObject.scene.name == "_Scene_1" || this.gameObject.scene.name == "_Scene_2") {
@@ -40,58 +40,55 @@ public class Player2 : ParentPlayer{
     }
 
     // Update is called once per frame
-    void Update(){
-         //variable declaration
-        if(!isDead){
+    void Update() {
+        if (!isDead) {
+            //variable declaration
             float horizontal = Input.GetAxis("Horizontal");
             float vertical = Input.GetAxis("Vertical");
 
-            //handles player attack
-            if(Time.time >= nextAttackTime) {
+            //handles player melee attack, if the time is greater than the next melee attack time then it will run
+            if (Time.time >= nextAttackTime) {
+                //if space has been pressed useMeleeAbility will run and the next attack time will be determined based off the attackRate
                 if (Input.GetKeyDown(KeyCode.Space)) {
                     useMeleeAbility(left);
                     nextAttackTime = Time.time + 1f / attackRate;
                 }
             }
 
-            if(Time.time >= nextShootTime) {
+            //handles player ranged attack, if the time is greater than the next ranged attack time then it will run
+            if (Time.time >= nextShootTime) {
+                //if B has been pressed useRangedAbility will run and the next ranged attack time will be determined based off the shootRate
                 if (Input.GetKeyDown(KeyCode.B)) {
                     useRangedAbility(left);
                     nextShootTime = Time.time + 1f / shootRate;
                 }
             }
 
-            //moves the player
+            //moves the player by calling the parent Move method and passes in the variables horizontal and vertical
             Move(horizontal, vertical);
 
-            //handles player animation
+            //handles player animation by calling the parent Animation method and passes in the variables horizontal and vertical
             Animation(horizontal, vertical);
 
-            //code used for player teleporting
+            //if the LeftShift key has been pressed the Teleport method will run and passes in the variables horizontal and vertical
             if (Input.GetKeyDown(KeyCode.LeftShift)) {
                 Teleport(horizontal, vertical);
             }
 
         }
 
-
-        //code used for invincibility
+        //if the E key has been pressed the Invincibility method will run
         if (Input.GetKeyDown(KeyCode.E)) {
             playerHealthBeforeInvincibility = playerHealth;
             Invincibility();
         }
 
-        //code used for activating a damage boost
+        //if the Q key has been pressed the DamageBoost method will run
         if (Input.GetKeyDown(KeyCode.Q)) {
             DamageBoost();
         }
 
-        //code for testing death
-        if(Input.GetKeyDown(KeyCode.P)) {
-            Die();
-        }
-
-        //after 30 seconds invincibility ends
+        //after 15 seconds invincibility ends by calling the HealthReset method and passes in the player's health before invincibility was activated
         if (healthResetCooldown <= 0) {
             HealthReset(playerHealthBeforeInvincibility);
         }
@@ -102,10 +99,12 @@ public class Player2 : ParentPlayer{
         healthResetCooldown -= Time.deltaTime;
         damageBoostCooldown -= Time.deltaTime;
     }
+
     //resets the game and re-initializes everything
     protected override void Reset() {
+        //loads the first scene, resets the cooldowns, resets the score, and resets the player health based on the maxHealth
         SceneManager.LoadScene("_Scene_1");
-        playerHealth = 80;
+        playerHealth = maxHealth;
         damageBoostCooldown = 0;
         healthResetCooldown = 100000;
         invincibilityCooldown = 0;
--- Assets/__Scripts/Player3.cs
+++ Assets/__Scripts/Player3.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class Player3 : ParentPlayer {
 
-    // Start is called before the first frame update
+    // Awake is called when the script instance is being loaded
     void Awake() {
 
         //initializes player health depending on the scene
@@ -41,58 +41,54 @@ public class Player3 : ParentPlayer {
 
     // Update is called once per frame
     void Update() {
-        //variable declaration
         if (!isDead) {
+            //variable declaration
             float horizontal = Input.GetAxis("Horizontal");
             float vertical = Input.GetAxis("Vertical");
 
-            //handles player attack
+            //handles player melee attack, if the time is greater than the next melee attack time then it will run
             if (Time.time >= nextAttackTime) {
+                //if space has been pressed useMeleeAbility will run and the next attack time will be determined based off the attackRate
                 if (Input.GetKeyDown(KeyCode.Space)) {
                     useMeleeAbility(left);
                     nextAttackTime = Time.time + 1f / attackRate;
                 }
             }
 
+            //handles player ranged attack, if the time is greater than the next ranged attack time then it will run
             if (Time.time >= nextShootTime) {
+                //if B has been pressed useRangedAbility will run and the next ranged attack time will be determined based off the shootRate
                 if (Input.GetKeyDown(KeyCode.B)) {
                     useRangedAbility(left);
                     nextShootTime = Time.time + 1f / shootRate;
                 }
             }
 
-
-            //moves the player
+            //moves the player by calling the parent Move method and passes in the variables horizontal and vertical
             Move(horizontal, vertical);
 
-            //handles player animation
+            //moves the player by calling the parent Move method and passes in the variables horizontal and vertical
             Animation(horizontal, vertical);
 
-            //code used for player teleporting
+            //if the LeftShift key has been pressed the Teleport method will run and passes in the variables horizontal and vertical
             if (Input.GetKeyDown(KeyCode.LeftShift)) {
                 Teleport(horizontal, vertical);
             }
 
         }
 
-
-        //code used for invincibility
+        //if the E key has been pressed the Invincibility method will run
         if (Input.GetKeyDown(KeyCode.E)) {
             playerHealthBeforeInvincibility = playerHealth;
             Invincibility();
         }
 
-        //code used for activating a damage boost
+        //if the Q key has been pressed the DamageBoost method will run
         if (Input.GetKeyDown(KeyCode.Q)) {
             DamageBoost();
         }
 
-        //code for testing death
-        if (Input.GetKeyDown(KeyCode.P)) {
-            Die();
-        }
-
-        //after 30 seconds invincibility ends
+        //after 15 seconds invincibility ends by calling the HealthReset method and passes in the player's health before invincibility was activated
         if (healthResetCooldown <= 0) {
             HealthReset(playerHealthBeforeInvincibility);
         }
@@ -103,10 +99,12 @@ public class Player3 : ParentPlayer {
         healthResetCooldown -= Time.deltaTime;
         damageBoostCooldown -= Time.deltaTime;
     }
+
     //resets the game and re-initializes everything
     protected override void Reset() {
+        //loads the first scene, resets the cooldowns, resets the score, and resets the player health based on the maxHealth
         SceneManager.LoadScene("_Scene_1");
-        playerHealth = 140;
+        playerHealth = maxHealth;
         damageBoostCooldown = 0;
         healthResetCooldown = 100000;
         invincibilityCooldown = 0;
