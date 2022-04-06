0db6b66f346159c72230f3c7bec0c54b8f4431da
kenangranulo <90563140+kenangranulo@users.noreply.github.com>
2022-04-05 20:38:55 -0400

    Comments
    Comments

--- Assets/__Scripts/MeleeAbility.cs
+++ Assets/__Scripts/MeleeAbility.cs
@@ -5,17 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class MeleeAbility : MonoBehaviour
 {
+    //String which will hold the screen variable
     public string screen;
 
+    //Will be called when the player selects stab
     public void ChooseStab()
     {
+        //Sets the string to the following values.
         PlayerPrefs.SetString("Attack", "Stab");
+        //Loads the new scene.
         SceneManager.LoadScene(screen);
     }
 
+    //Will be called when the player selects slash.
     public void ChooseSlash()
     {
+        //Sets the string to the following values. 
         PlayerPrefs.SetString("Attack", "Slash");
+        //Loads the new scene
         SceneManager.LoadScene(screen);
     }
 }
