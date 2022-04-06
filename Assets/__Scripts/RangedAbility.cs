26e8ee056cf5b6b852000d86b49a06d737e95002
Rohan Datta <rohandatta02@gmail.com>
2022-04-05 21:54:27 -0400

    Adding comments
    Commented rangedability and storyscreen

--- Assets/__Scripts/RangedAbility.cs
+++ Assets/__Scripts/RangedAbility.cs
@@ -5,23 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class RangedAbility : MonoBehaviour
 {
+    //String to hold the name of the next scene
     public string screen;
 
+    //Called when the gun button is selected
     public void ChooseGun()
     {
+        //Sets the players ranged ability to gun
         PlayerPrefs.SetString("Ranged","Gun");
+        //Goes to the next scene
         SceneManager.LoadScene(screen);
     }
 
+    //Called when the bomb button is selected
     public void ChooseSplashBomb()
     {
+        //Sets the players ranged ability to bomb
         PlayerPrefs.SetString("Ranged","Bomb");
-        SceneManager.LoadScene(screen);
-    }
-
-    public void ChooseGrapplingHook()
-    {
-        PlayerPrefs.SetString("Ranged","Hook");
+        //Goes to the next scene
         SceneManager.LoadScene(screen);
     }
 }
--- Assets/__Scripts/StoryScreen.cs
+++ Assets/__Scripts/StoryScreen.cs
@@ -5,10 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class StoryScreen : MonoBehaviour
 {
+    //The string holds the variable for the name of the scene that is going to be changed to
     public string screen;
 
+    //Method gets called when the button is clicked
     public void PressOk()
     {
+        //Switches scene based on the string value that is defined in Unity
         SceneManager.LoadScene(screen);
     }
 }
